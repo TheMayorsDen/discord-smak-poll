8155c2975f7c15a7a3b0abbd0af7852df9efddc7
TheMayorsDen <operationmossley@gmail.com>
2026-09-22 05:09:59 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const {
+    Client,
+    GatewayIntentBits,
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    EmbedBuilder
+} = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds
+    ]
+});
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
